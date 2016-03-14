51f6a6c396c7ed8bbd4443323926aaa2ffbdb519
mcasper3 <mikecasper395@gmail.com>
2016-03-13 23:06:11 -0500

    added log in view

--- app/src/main/java/me/mikecasper/musicvoice/LogInActivity.java
+++ app/src/main/java/me/mikecasper/musicvoice/LogInActivity.java
@@ -1,13 +1,76 @@
 package me.mikecasper.musicvoice;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 9001;
+    private static final String REDIRECT_URI = "mikecasper.me://callback";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        ImageView logInButton = (ImageView) findViewById(R.id.logInButton);
+
+        if (logInButton != null) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                Drawable image = ContextCompat.getDrawable(this, R.drawable.log_in_button);
+                RippleDrawable drawable = new RippleDrawable(ColorStateList.valueOf(ContextCompat.getColor(this, R.color.spotify_green_pressed)), image, null);
+                logInButton.setImageDrawable(drawable);
+            }
+
+            logInButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onLogIn();
+                }
+            });
+        }
+    }
+
+    private void onLogIn() {
+        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder("clientId", AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+
+        builder.setScopes(new String[] { "streaming" });
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+
+            switch (response.getType()) {
+                case TOKEN:
+                    // TODO go to next page
+                    break;
+                case ERROR:
+                    Log.e("DS", "DSDSF");
+                    break;
+                default:
+            }
+        }
     }
 }
