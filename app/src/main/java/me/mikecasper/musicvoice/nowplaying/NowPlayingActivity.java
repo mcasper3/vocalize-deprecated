8852ef68b7e12d4d748b0dc7afa3b94228fb546e
mcasper3 <mikecasper395@gmail.com>
2016-05-03 00:24:19 -0500

    added button actions

--- app/src/main/java/me/mikecasper/musicvoice/nowplaying/NowPlayingActivity.java
+++ app/src/main/java/me/mikecasper/musicvoice/nowplaying/NowPlayingActivity.java
@@ -18,15 +18,26 @@ import me.mikecasper.musicvoice.R;
 import me.mikecasper.musicvoice.models.Track;
 import me.mikecasper.musicvoice.services.eventmanager.EventManagerProvider;
 import me.mikecasper.musicvoice.services.eventmanager.IEventManager;
+import me.mikecasper.musicvoice.services.musicplayer.events.SkipBackwardEvent;
+import me.mikecasper.musicvoice.services.musicplayer.events.SkipForwardEvent;
+import me.mikecasper.musicvoice.services.musicplayer.events.TogglePlaybackEvent;
+import me.mikecasper.musicvoice.services.musicplayer.events.ToggleRepeatEvent;
+import me.mikecasper.musicvoice.services.musicplayer.events.ToggleShuffleEvent;
 import me.mikecasper.musicvoice.track.TrackFragment;
 
 public class NowPlayingActivity extends MusicVoiceActivity {
+
+    private static final int MODE_DISABLED = 0;
+    private static final int MODE_ENABLED = 1;
+    private static final int MODE_SINGLE = 2;
 
     // Services
     private IEventManager mEventManager;
 
     // Data Members
     private boolean mIsPlayingMusic;
+    private boolean mShuffleEnabled;
+    private int mRepeatMode;
 
     public NowPlayingActivity() {
         // Required empty public constructor
@@ -40,13 +51,7 @@ public class NowPlayingActivity extends MusicVoiceActivity {
 
         mEventManager = EventManagerProvider.getInstance(this);
 
-        ImageView playPauseButton = (ImageView) findViewById(R.id.playPauseButton);
-        playPauseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updatePlayButton();
-            }
-        });
+        setUpButtons();
 
         ImageView albumArt = (ImageView) findViewById(R.id.albumArt);
         TextView trackName = (TextView) findViewById(R.id.trackName);
@@ -66,6 +71,86 @@ public class NowPlayingActivity extends MusicVoiceActivity {
 
             trackName.setText(track.getName());
             artistName.setText(track.getArtists().get(0).getName());
+        }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    private void setUpButtons() {
+        View playPauseButton = findViewById(R.id.playPauseButton);
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEventManager.postEvent(new TogglePlaybackEvent());
+                updatePlayButton();
+            }
+        });
+
+        View skipForwardButton = findViewById(R.id.skipNextButton);
+        skipForwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEventManager.postEvent(new SkipForwardEvent());
+            }
+        });
+
+        View skipBackwardButton = findViewById(R.id.skipPreviousButton);
+        skipBackwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEventManager.postEvent(new SkipBackwardEvent());
+            }
+        });
+
+        View repeatButton = findViewById(R.id.repeatButton);
+        repeatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEventManager.postEvent(new ToggleRepeatEvent());
+                updateRepeatButton();
+            }
+        });
+
+        View shuffleButton = findViewById(R.id.shuffleButton);
+        shuffleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mEventManager.postEvent(new ToggleShuffleEvent());
+                updateShuffleButton();
+            }
+        });
+    }
+
+    private void updateShuffleButton() {
+        ImageView shuffleButton = (ImageView) findViewById(R.id.shuffleButton);
+
+        if (shuffleButton != null) {
+            if (mShuffleEnabled) {
+                shuffleButton.setImageResource(R.drawable.ic_shuffle_disabled);
+            } else {
+                shuffleButton.setImageResource(R.drawable.ic_shuffle);
+            }
+
+            mShuffleEnabled = !mShuffleEnabled;
+            // TODO update in shared prefs
+        }
+    }
+
+    private void updateRepeatButton() {
+        ImageView repeatButton = (ImageView) findViewById(R.id.repeatButton);
+
+        if (repeatButton != null) {
+            if (mRepeatMode == MODE_DISABLED) {
+                repeatButton.setImageResource(R.drawable.ic_repeat);
+                mRepeatMode = MODE_ENABLED;
+            } else if (mRepeatMode == MODE_ENABLED) {
+                repeatButton.setImageResource(R.drawable.ic_repeat_song);
+                mRepeatMode = MODE_SINGLE;
+            } else {
+                repeatButton.setImageResource(R.drawable.ic_repeat_disabled);
+                mRepeatMode = MODE_DISABLED;
+            }
+
+            // TODO update shared prefs
         }
     }
 
